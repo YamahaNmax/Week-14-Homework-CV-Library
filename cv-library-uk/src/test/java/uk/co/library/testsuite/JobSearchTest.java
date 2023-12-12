6997c885a62c38ff378e3ca8f61cb7fa0a23ff7f
YamahaNmax <m4mitzlearning@gmail.com>
2023-12-12 13:41:24 +0000

    Push

--- cv-library-uk/src/test/java/uk/co/library/testsuite/JobSearchTest.java
+++ cv-library-uk/src/test/java/uk/co/library/testsuite/JobSearchTest.java
@@ -52,4 +52,105 @@ public class JobSearchTest extends TestBase {
 
     }
 
+    @Test
+    public void verifyJobSearchResultUsingDifferentDataSet3() {
+
+        homePage.enterJobTitle("Project Manager");
+        homePage.enterLocation("leeds");
+        homePage.selectDistance("7");
+        homePage.clickOnMoreSearchOptionLink();
+        homePage.enterMinSalary("300");
+        homePage.enterMaxSalary("950");
+        homePage.selectSalaryType("Per day");
+        homePage.selectJobType("Contract");
+        homePage.clickOnFindJobsButton();
+
+        String actualText = resultPage.verifyTheResults();
+        String ExpectedText = "Contract Project Manager jobs in Leeds";
+        System.out.println(actualText);
+        Assert.assertEquals(actualText, ExpectedText);
+
+    }
+
+    @Test
+    public void verifyJobSearchResultUsingDifferentDataSet4() {
+
+        homePage.enterJobTitle("Software Engineer");
+        homePage.enterLocation("Reading");
+        homePage.selectDistance("50");
+        homePage.clickOnMoreSearchOptionLink();
+        homePage.enterMinSalary("24");
+        homePage.enterMaxSalary("60");
+        homePage.selectSalaryType("Per hour");
+        homePage.selectJobType("Permanent");
+        homePage.clickOnFindJobsButton();
+
+        String actualText = resultPage.verifyTheResults();
+        String ExpectedText = "Permanent Software Engineer jobs in Reading";
+        System.out.println(actualText);
+        Assert.assertEquals(actualText, ExpectedText);
+
+    }
+
+    @Test
+    public void verifyJobSearchResultUsingDifferentDataSet5() {
+
+        homePage.enterJobTitle("Risk Manager");
+        homePage.enterLocation("London");
+        homePage.selectDistance("2");
+        homePage.clickOnMoreSearchOptionLink();
+        homePage.enterMinSalary("1200");
+        homePage.enterMaxSalary("2400");
+        homePage.selectSalaryType("Per week");
+        homePage.selectJobType("Temporary");
+        homePage.clickOnFindJobsButton();
+
+        String actualText = resultPage.verifyTheResults();
+        String ExpectedText = "Temporary Risk Manager jobs in London";
+        System.out.println(actualText);
+        Assert.assertEquals(actualText, ExpectedText);
+
+    }
+
+    @Test
+    public void verifyJobSearchResultUsingDifferentDataSet6() {
+
+        homePage.enterJobTitle("Aws Engineer");
+        homePage.enterLocation("Manchester");
+        homePage.selectDistance("75");
+        homePage.clickOnMoreSearchOptionLink();
+        homePage.enterMinSalary("4000");
+        homePage.enterMaxSalary("8500");
+        homePage.selectSalaryType("Per month");
+        homePage.selectJobType("Permanent");
+        homePage.clickOnFindJobsButton();
+
+        String actualText = resultPage.verifyTheResults();
+        String ExpectedText = "Permanent Aws Engineer jobs in Manchester";
+        System.out.println(actualText);
+        Assert.assertEquals(actualText, ExpectedText);
+
+    }
+
+    @Test
+    public void verifyJobSearchResultUsingDifferentDataSet7() {
+
+        homePage.enterJobTitle("SIA Guard");
+        homePage.enterLocation("City of London");
+        homePage.selectDistance("100");
+        homePage.clickOnMoreSearchOptionLink();
+        homePage.enterMinSalary("11");
+        homePage.enterMaxSalary("17");
+        homePage.selectSalaryType("Per hour");
+        homePage.selectJobType("Part Time");
+        homePage.clickOnFindJobsButton();
+
+        String actualText = resultPage.verifyTheResults();
+        String ExpectedText = "Part Time Sia Guard jobs in City of London";
+        System.out.println(actualText);
+        Assert.assertEquals(actualText, ExpectedText);
+
+    }
+
+
 }
